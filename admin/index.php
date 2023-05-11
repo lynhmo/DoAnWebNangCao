320a0fbe5ccfc8b9d1e75e3a5d68c3aa76c009ae
Lynh <chinsugaming@gmail.com>
2023-05-11 21:55:50 +0700

    update chart

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,19 @@
 <?php
-    include("../dbConnection.php");
-    $dbConnection = new dbConnection();
-    $conn = $dbConnection->getConnection();
-    $sql = "SELECT DATE(updatedate) AS ngay, SUM(total_money) AS tongtien
+include("../dbConnection.php");
+$dbConnection = new dbConnection();
+$conn = $dbConnection->getConnection();
+$sql = "SELECT DATE(updatedate) AS ngay, SUM(total_money) AS tongtien
             FROM checkout
-            GROUP BY ngay;";
-    // Thực hiện truy vấn data thông qua hàm mysqli_query
-    $query = mysqli_query($conn,$sql);
-    $data = array(array('DATE', 'TOTAL_MONEY'));
-    while ($row = mysqli_fetch_assoc($query)) 
-    {
-        $data[] = array($row['ngay'], (int) $row['tongtien']);
-    }
+            GROUP BY ngay
+            ORDER BY ngay
+            ASC
+            LIMIT 30;";
+// Thực hiện truy vấn data thông qua hàm mysqli_query
+$query = mysqli_query($conn, $sql);
+$data = array(array('DATE', 'TOTAL_MONEY'));
+while ($row = mysqli_fetch_assoc($query)) {
+    $data[] = array($row['ngay'], (int) $row['tongtien']);
+}
 ?>
 <!-- 
 Đoạn back up    
@@ -41,23 +43,28 @@
 </script> -->
 
 <script type="text/javascript">
-    google.charts.load('current', {'packages':['corechart']});
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
     google.charts.setOnLoadCallback(drawChart);
+
     function drawChart() {
 
-    var data = google.visualization.arrayToDataTable(
-        <?php echo json_encode($data); ?>
-    );
+        var data = google.visualization.arrayToDataTable(
+            <?php echo json_encode($data); ?>
+        );
 
-    var options = {
-        title: 'Báo Cáo Thống Kê',
-        curveType: 'function',
-        legend: { position: 'bottom' }
-    };
+        var options = {
+            title: 'Báo Cáo Thống Kê',
+            curveType: 'function',
+            legend: {
+                position: 'bottom'
+            }
+        };
 
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
-    chart.draw(data, options);
+        chart.draw(data, options);
     }
 </script>
 <div>
