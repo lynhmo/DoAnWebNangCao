70f3f3f3e468d2002447ee820c824d359c688974
Lynh <chinsugaming@gmail.com>
2023-06-01 08:46:28 +0700

    update ten tab

--- admin/home.php
+++ admin/home.php
@@ -11,7 +11,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <title>Document</title>
+    <title>ADMIN PAGE</title>
 </head>
 
 <body>
